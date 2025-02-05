2e1108eed556a1ddd037d9c39bfeedc5858ca708
Péntek József <tafaya@users.noreply.github.com>
2025-02-05 11:26:20 +0100

    Docker compose

--- src/main/java/com/pj/demo/DemoApplication.java
+++ src/main/java/com/pj/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.pj.demo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Args: " + Arrays.toString(args));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
